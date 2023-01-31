1c50e8c6c1bb3a0787d79e0eb636b966e1b37d28
Payton <payton_campbell1@baylor.edu>
2023-01-30 19:48:25 -0600

    Review

--- customer_sales_report.py
+++ customer_sales_report.py
@@ -2,17 +2,34 @@ import csv
 
 
 def main():
-    infile = open('sales.csv', 'r')
+    infile = open('sales.csv', 'r', newline='')
+    reader = csv.reader(infile)
+    next(reader)
+    outfile = open('salesreport.csv', 'w', newline='')
 
-    csvfile = csv.reader(infile, delimiter=',')
-    next(csvfile)
+    writer = csv.writer(outfile)
+    writer.writerow(["Customer ID, Total"])
 
-    with open("salesreport.csv", 'a') as outfile:
-        outfile.write('Customer ID, Total\n')
-        for record in csvfile:
-            CustID = record[0]
-            Sales = record[3]
-            outfile.write(CustID + ',' + Sales + "\n")
+    customer = ""
+    count = 0
+    total = 0
+
+    for record in reader:
+        if count == 0:
+            customer = record[0]
+        if record[0] == customer:
+            total += float(record[3]) - (float(record[4]) + float(record[5]))
+
+        else:
+            writer.writerow([customer, format(total, '.2f')])
+            total = float(record[3]) - (float(record[4]) + float(record[5]))
+        customer = record[0]
+        count += 1
+
+    writer.writerow([customer,  format(total, '.2f')])
+
+    infile.close()
+    outfile.close()
 
 
 main()
